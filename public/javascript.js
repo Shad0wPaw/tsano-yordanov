ee17e379c6b6ea41884e1394c00d4976d4366a95
Tsanko Yordanov <faxyhitz1@gmail.com>
2022-11-22 14:46:50 +0000

    home page and db upd

--- public/javascript.js
+++ public/javascript.js
@@ -1,29 +1,32 @@
-
 ////////////////////////////////////// post
 if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(async position => {
-        var lat = position.coords.latitude
-        var long = position.coords.longitude
-4
-        const dataLocation = { lat,long }
-        const options = {
-            method: 'POST',
-            headers: { "Content-Type" : "application/json" } ,
-            body: JSON.stringify(dataLocation)
-        }
-        const response = await fetch('/api', options);
-        const successJson = await response.json();
-      });
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    4;
+    const dataLocation = { lat, long };
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataLocation),
+    };
+    const response = await fetch("/api", options);
+    const successJson = await response.json();
+  });
 } else {
-    console.log('Location Access Denied')
+  console.log("Location Access Denied");
 }
 
 ////////////////////////////////////// get
 getFetch();
-async function getFetch(){
-    const response = await fetch('/api')
-    const data = await response.json()
-    for (d of data) {
-        document.getElementById('root_checkout').innerHTML += `<div><h3><b>ID: ${d._id} <br>Entered at: ${d.time}<b></h3><br> </div>`
-    }
+async function getFetch() {
+  const response = await fetch("/api");
+  const data = await response.json();
+  for (d of data) {
+    document.getElementById(
+      "root_checkout"
+    ).innerHTML += `<div><h3><b>User Login: <a href="https://www.latlong.net/c/?lat=${d.lat}&long=${d.long}" target="_blank">(${d.lat}, ${d.long})</a><br>
+      <b></h3><br> </div>
+           `;
+  }
 }
