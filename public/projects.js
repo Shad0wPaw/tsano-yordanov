a945f076fc52a0c368fae350af0bb8b1a492e2cf
Tsanko Yordanov <faxyhitz1@gmail.com>
2022-12-21 13:37:48 +0000

    commit vv1

--- public/projects.js
+++ public/projects.js
@@ -1,9 +1,9 @@
 const cardsObject = document.getElementsByClassName("card-selector")
-const mainPagePr = document.getElementsByClassName("projects")[0]
 
+function goToProject(){
+    location.replace("productCrystal.html")
+}
 
 for(a=0; a<cardsObject.length; a++){
- cardsObject[a].addEventListener('click', ()=>{
-    location.replace("productCrystal.html")
-})
+ cardsObject[a].addEventListener('click', goToProject)
 }
