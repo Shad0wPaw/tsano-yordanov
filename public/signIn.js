b8f1ada389a7eeeac9c1b76ce1d486e40c976a43
Tsanko Yordanov <faxyhitz1@gmail.com>
2022-12-23 13:21:56 +0000

    NEW NEW AND NEWEST VERSION USE THIS

--- public/signIn.js
+++ public/signIn.js
@@ -2,7 +2,9 @@
 //document.forms[0].elements.form2Example2.addEventListener('input', ()=> {console.log(document.forms[0].elements.form2Example2.value)})
 // document.forms[0].elements.form2Example1.addEventListener('change', ()=> {console.log(document.forms[0].elements.form2Example1.value)})
 
-const userInput = document.forms[0].elements.form2Example1;
+const loginForm = document.getElementById('login');
+const emailInputG = loginForm.elements.emailLogin;
+const passwordInputG = loginForm.elements.passwordLogin;
 
 function addEvent(el, event, callback) {
     if('addEventListener' in el){
@@ -17,13 +19,20 @@ function addEvent(el, event, callback) {
   }
 
 (function(){
-const loginForm = document.getElementById('login');
 
 addEvent(loginForm, 'submit', function(e){
     e.preventDefault();
     var elements = this.elements;
     var email = elements.emailLogin.value
     var password = elements.passwordLogin.value
-    console.log(email + ' ' + password)
+    console.log(userInput)
 })
+}())
+
+(function(){
+    var submitted = false;
+    
+    submit.
+
+    
 }())
